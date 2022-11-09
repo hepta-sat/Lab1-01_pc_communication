80f0ebe2c9ab4df8f322495328693752102d1958
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-11-09 20:30:14 +0000

    changed to rawserial

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
-#include "mbed.h"
+#include"mbed.h"
 
-DigitalOut myled(LED1);
+RawSerial pc(USBTX,USBRX);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main(){
+    int i1=7;
+    float i2=7.5;
+    char i3='H';
+    char c;
+    
+    pc.printf("Hello World!\r\n");
+    pc.printf("i1=%d\r\n",i1);
+    pc.printf("i2=%f\r\n",i2);
+    pc.printf("i3=%c\r\n",i3);
+    pc.putc(i3);
+    pc.printf("\r\n");
+    
+    for(int j=0; j<10; j++){
+        pc.printf("j=%d -- enter any key --> ",j);
+        c = pc.getc();
+        pc.putc(c);
+        pc.printf("\r\n");
+    }
+    
+    pc.printf("finish!!\r\n");
 }
